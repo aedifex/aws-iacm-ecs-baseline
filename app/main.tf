3104137b7b861c46f25570fe44fc525d8ff48171
aedifex <aedifex@gmail.com>
2026-03-02 21:35:52 -0800

    Update folder structure

--- app/main.tf
+++ app/main.tf
@@ -18,23 +18,7 @@ provider "aws" {
 }
 
 ############################################
-# Variables
-############################################
-
-variable "container_image" {
-  description = "Container image for ECS task"
-  type        = string
-  default     = "nginx:latest"
-}
-
-variable "desired_count" {
-  description = "Number of running tasks"
-  type        = number
-  default     = 1
-}
-
-############################################
-# Data Sources (Reference Infra Workspace)
+# Default Networking (use default VPC)
 ############################################
 
 data "aws_vpc" "default" {
@@ -48,12 +32,12 @@ data "aws_subnets" "default" {
   }
 }
 
+############################################
+# Reference existing infra resources
+############################################
+
 data "aws_ecs_cluster" "main" {
   cluster_name = "hx-ecs-cluster"
-}
-
-data "aws_lb_target_group" "tg" {
-  name = "hx-ecs-tg"
 }
 
 data "aws_security_group" "ecs_sg" {
@@ -64,12 +48,66 @@ data "aws_iam_role" "execution_role" {
   name = "hx-ecs-task-execution-role"
 }
 
+data "aws_lb" "alb" {
+  name = "hx-ecs-alb"
+}
+
+data "aws_lb_listener" "http" {
+  load_balancer_arn = data.aws_lb.alb.arn
+  port              = 80
+}
+
 ############################################
-# ECS Task Definition
+# NEW Target Group for NGINX (port 80)
+############################################
+
+resource "aws_lb_target_group" "nginx_tg" {
+  name        = "hx-ecs-tg-nginx"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = data.aws_vpc.default.id
+  target_type = "ip"
+
+  deregistration_delay = 30
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200-399"
+    interval            = 15
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+############################################
+# Listener Rule to route ALL traffic to nginx
+# (Overrides infra default action without editing infra)
+############################################
+
+resource "aws_lb_listener_rule" "nginx_all" {
+  listener_arn = data.aws_lb_listener.http.arn
+  priority     = 1
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.nginx_tg.arn
+  }
+
+  condition {
+    path_pattern {
+      values = ["/*"]
+    }
+  }
+}
+
+############################################
+# ECS Task Definition (NGINX)
 ############################################
 
 resource "aws_ecs_task_definition" "task" {
-  family                   = "hx-ecs-task"
+  family                   = "hx-ecs-nginx"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   cpu                      = "256"
@@ -78,14 +116,12 @@ resource "aws_ecs_task_definition" "task" {
 
   container_definitions = jsonencode([
     {
-      name      = "nginx"
-      image     = var.container_image
+      name      = "app"
+      image     = "nginx:latest"
       essential = true
+
       portMappings = [
-        {
-          containerPort = 80
-          hostPort      = 80
-        }
+        { containerPort = 80 }
       ]
     }
   ])
@@ -96,11 +132,13 @@ resource "aws_ecs_task_definition" "task" {
 ############################################
 
 resource "aws_ecs_service" "service" {
-  name            = "hx-ecs-service"
+  name            = "hx-ecs-service-nginx"
   cluster         = data.aws_ecs_cluster.main.id
   task_definition = aws_ecs_task_definition.task.arn
-  desired_count   = var.desired_count
+  desired_count   = 1
   launch_type     = "FARGATE"
+
+  health_check_grace_period_seconds = 60
 
   network_configuration {
     subnets          = data.aws_subnets.default.ids
@@ -109,21 +147,17 @@ resource "aws_ecs_service" "service" {
   }
 
   load_balancer {
-    target_group_arn = data.aws_lb_target_group.tg.arn
-    container_name   = "nginx"
+    target_group_arn = aws_lb_target_group.nginx_tg.arn
+    container_name   = "app"
     container_port   = 80
   }
 
-  depends_on = [aws_ecs_task_definition.task]
+  depends_on = [aws_lb_listener_rule.nginx_all]
 }
 
 ############################################
-# Output ALB DNS
+# Output
 ############################################
-
-data "aws_lb" "alb" {
-  name = "hx-ecs-alb"
-}
 
 output "alb_dns_name" {
   value = data.aws_lb.alb.dns_name
--- infra/main.tf
+++ infra/main.tf
@@ -102,6 +102,14 @@ resource "aws_security_group" "ecs_sg" {
     cidr_blocks = ["0.0.0.0/0"] # POV only
   }
 
+  ingress {
+    description = "Allow app traffic"
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
@@ -134,11 +142,12 @@ resource "aws_lb" "alb" {
 ############################################
 
 resource "aws_lb_target_group" "tg" {
-  name        = "hx-ecs-tg"
-  port        = 80
-  protocol    = "HTTP"
-  vpc_id      = data.aws_vpc.default.id
-  target_type = "ip"
+  name                 = "hx-ecs-tg"
+  port                 = 3000
+  protocol             = "HTTP"
+  vpc_id               = data.aws_vpc.default.id
+  target_type          = "ip"
+  deregistration_delay = 30
 
   health_check {
     path                = "/"
